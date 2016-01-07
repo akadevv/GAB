b14e8a150aa6256e215b055edf237281edbec95c
LCD <pd6292@naver.com>
2016-01-07 15:24:21 +0900

    주소록 불러오기 미완성본

--- app/src/main/java/com/example/lcd/gab/FriendListMain.java
+++ app/src/main/java/com/example/lcd/gab/FriendListMain.java
@@ -5,6 +5,8 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import java.util.ArrayList;
 
@@ -14,55 +16,54 @@ import java.util.ArrayList;
 public class FriendListMain extends Activity{
 
     ArrayList<FriendData> friendDataList;
-    ArrayList<FriendData> list;
+    ArrayList<FriendData> list = new ArrayList<>();
     FriendData friendData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.friend_list_main);
-
+        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.friend_recyclerView);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(RecyclerView.VERTICAL);
+        recyclerView.setLayoutManager(layoutManager);
         getPhoneNumList();
+        ///System.out.println(friendDataList.get(0).getName()+","+friendDataList.get(0).getPhoneNum());
 
-
+        recyclerView.setAdapter(new FriendListAdapter(friendDataList));
     }
 
     public void getPhoneNumList() {
         Cursor cursor = null;
-        try {
-            Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
-            String phoneName = ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME;
-            String[] ad = new String[]{ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone.NUMBER};
-            cursor = getContentResolver().query(uri, ad, null, null, phoneName);
+        Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+        String phoneName = ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME;
+        String[] ad = new String[]{ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone.NUMBER};
+        cursor = getContentResolver().query(uri, ad, null, null, phoneName);
 
-            cursor.moveToFirst();
-            while(cursor.moveToNext()){
+            if(cursor.moveToFirst()) {
+                while (cursor.moveToNext()) {
 
-                if(cursor.getString(1)!=null){
-                    if(list.size() == 0){
-                        friendData = new FriendData();
-                        friendData.setName(cursor.getString(0));
-                        friendData.setPhoneNum(cursor.getString(1));
-                        list.add(friendData);
-                    }
-                    else{
-                        if(!list.get(list.size()-1).getPhoneNum().equals(cursor.getString(1))){
+                    if (cursor.getString(1) != null) {
+                        //System.out.println("zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz");
+                        if (list.size()==0) {
                             friendData = new FriendData();
                             friendData.setName(cursor.getString(0));
                             friendData.setPhoneNum(cursor.getString(1));
                             list.add(friendData);
+                        } else {
+                            if (!list.get(list.size() - 1).getPhoneNum().equals(cursor.getString(1))) {
+                                friendData = new FriendData();
+                                friendData.setName(cursor.getString(0));
+                                friendData.setPhoneNum(cursor.getString(1));
+                                list.add(friendData);
+                            }
                         }
                     }
-                }
+                }friendDataList = list;
             }
-            friendDataList = list;
-        }catch (Exception e){
-
-        }finally {
-            if(cursor != null){
-                cursor.close();
-                cursor = null;
-            }
+        cursor.close();
+        cursor = null;
         }
-    }
+
 }
