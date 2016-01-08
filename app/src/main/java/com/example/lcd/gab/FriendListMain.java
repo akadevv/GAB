89cc068431dfa0bc6d5e89b66cc764f36338a0db
LCD <pd6292@naver.com>
2016-01-08 11:29:59 +0900

    주소록 검색 시스템 추가(초성검색은 아직 안됨)

--- app/src/main/java/com/example/lcd/gab/FriendListMain.java
+++ app/src/main/java/com/example/lcd/gab/FriendListMain.java
@@ -18,20 +18,57 @@ public class FriendListMain extends Activity{
     ArrayList<FriendData> friendDataList;
     ArrayList<FriendData> list = new ArrayList<>();
     FriendData friendData;
+    RecyclerView recyclerView;
+    FriendListAdapter friendListAdapter;
+    android.widget.SearchView searchView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.friend_list_main);
 
-        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.friend_recyclerView);
+        recyclerView = (RecyclerView)findViewById(R.id.friend_recycler_view);
+        searchView = (android.widget.SearchView) findViewById(R.id.friend_search_view);
+
         recyclerView.setHasFixedSize(true);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         layoutManager.setOrientation(RecyclerView.VERTICAL);
         recyclerView.setLayoutManager(layoutManager);
+
         getPhoneNumList();
+
         recyclerView.setAdapter(new FriendListAdapter(friendDataList));
+
+        searchView.setOnQueryTextListener(listener);
     }
+
+        android.widget.SearchView.OnQueryTextListener listener = new android.widget.SearchView.OnQueryTextListener(){
+            @Override
+            public boolean onQueryTextChange(String query){
+                query = query.toLowerCase();
+                final ArrayList<FriendData> filteredList = new ArrayList<>();
+
+                for(int i=0; i< friendDataList.size(); i++){
+                    final String name = friendDataList.get(i).getName().toLowerCase();
+                    final String phoneNum = friendDataList.get(i).getPhoneNum();
+                    if(name.contains(query)){
+                        filteredList.add(friendDataList.get(i));
+                    }
+                    else if(phoneNum.contains(query)){
+                        filteredList.add(friendDataList.get(i));
+                    }
+                }
+                recyclerView.setLayoutManager(new LinearLayoutManager(FriendListMain.this));
+                friendListAdapter = new FriendListAdapter(filteredList);
+                recyclerView.setAdapter(friendListAdapter);
+                friendListAdapter.notifyDataSetChanged();
+                return true;
+            }
+            public boolean onQueryTextSubmit(String query){
+                return false;
+            }
+        };
 
     public void getPhoneNumList() {
         Cursor cursor = null;
